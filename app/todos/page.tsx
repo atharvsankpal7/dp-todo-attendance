3f3c7dc912445036f79cbcb114c880ec2963a3b6
Atharv <110733438+atharvsankpal7@users.noreply.github.com>
2025-05-10 16:00:05 +0530

    fix : suspense boundary in todos/page.tsx

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 import TodosContent from "@/components/todo/todo-content";
+import { Loader2 } from "lucide-react";
+import { Suspense } from "react";
 
 
 export default function TodosPage() {
   return (
-    // <Suspense fallback={
-    //   <div className="container py-10">
-    //     <div className="flex justify-center items-center min-h-[50vh]">
-    //       <div className="flex flex-col items-center">
-    //         <Loader2 className="h-8 w-8 animate-spin mb-4" />
-    //         <p>Loading todos...</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    // }>
-    //   <TodosContent />
-    // </Suspense>
-    <TodosContent/>
+    <Suspense fallback={
+      <div className="container py-10">
+        <div className="flex justify-center items-center min-h-[50vh]">
+          <div className="flex flex-col items-center">
+            <Loader2 className="h-8 w-8 animate-spin mb-4" />
+            <p>Loading todos...</p>
+          </div>
+        </div>
+      </div>
+    }>
+      <TodosContent />
+    </Suspense>
+    // <TodosContent/>
   );
 }
